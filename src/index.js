f9bbd1eff811df5abc3c8df67011a05911f09d88
Zach Alam <zelphics@gmail.com>
2020-05-24 13:00:04 -0700

    prettier

--- src/index.js
+++ src/index.js
@@ -1,225 +1,265 @@
 const isNode =
-  typeof process !== 'undefined' &&
+  typeof process !== "undefined" &&
   process.versions != null &&
   process.versions.node != null;
 
-const { Api, JsonRpc } = require('eosjs');
-const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
-const eosECC = require('eosjs-ecc');
+const { Api, JsonRpc } = require("eosjs");
+const { JsSignatureProvider } = require("eosjs/dist/eosjs-jssig");
+const eosECC = require("eosjs-ecc");
 
-let fetch
-if(isNode) {
-    fetch = require('node-fetch')
-    const util = require('util');
-    TextDecoder = util.TextDecoder;
-    TextEncoder = util.TextEncoder;
+let fetch;
+if (isNode) {
+  fetch = require("node-fetch");
+  const util = require("util");
+  TextDecoder = util.TextDecoder;
+  TextEncoder = util.TextEncoder;
 }
 
 module.exports = function ({
-        account_name, // required
-        eos_endpoint, // required
-        contract = 'putinventory', // defaults to 'putinventory' string (optional)
-        account_payer_pk = '', // defaults to empty string (optional)
-        account_payer_permission = 'active' // defaults to active. (optional)
-    }) {
+  account_name, // required
+  eos_endpoint, // required
+  contract = "putinventory", // defaults to 'putinventory' string (optional)
+  account_payer_pk = "", // defaults to empty string (optional)
+  account_payer_permission = "active", // defaults to active. (optional)
+}) {
+  this.account_name = account_name;
+  this.eos_endpoint = eos_endpoint;
+  this.contract = contract;
+  this.account_payer_pk = account_payer_pk;
+  this.account_payer_permission = account_payer_permission;
+  this.defaultAuth = [
+    { actor: this.account_name, permission: this.account_payer_permission },
+  ];
+  this.fixedRowRamCost = 284;
 
-    this.account_name = account_name;
-    this.eos_endpoint = eos_endpoint;
-    this.contract = contract;
-    this.account_payer_pk = account_payer_pk;
-    this.account_payer_permission = account_payer_permission;
-    this.defaultAuth = [{ actor: this.account_name, permission: this.account_payer_permission}];
-    this.fixedRowRamCost = 284;
+  this.rpc = new JsonRpc(eos_endpoint, isNode ? { fetch } : {});
+  this.eos = new Api({
+    rpc: this.rpc,
+    signatureProvider: new JsSignatureProvider([this.account_payer_pk]),
+    textEncoder: new TextEncoder(),
+    textDecoder: new TextDecoder(),
+  });
 
-    this.rpc = new JsonRpc(eos_endpoint, isNode ? { fetch } : {});
-    this.eos = new Api({
-        rpc: this.rpc,
-        signatureProvider: new JsSignatureProvider([this.account_payer_pk]),
-        textEncoder: new TextEncoder(),
-        textDecoder: new TextDecoder()
+  this.keypair = async function () {
+    // generates a new (random) keypair
+    return {
+      ownerPrivateKey: await eosECC.randomKey(),
+      activePrivateKey: await eosECC.randomKey(),
+    };
+  };
+
+  this.verify = async function (privateKey, accountName) {
+    // verifies a given accountName can sign with privateKey
+    const account = await this.account(accountName);
+    const publicKey = eosECC.privateToPublic(privateKey);
+    return account.permissions.some((p) =>
+      p.required_auth.keys.some((k) => k.key == publicKey)
+    );
+  };
+
+  this.account = function () {
+    // returns an account
+    return this.rpc.get_account(this.account_name);
+  };
+
+  this.ram = async function () {
+    // returns ram usage for an account
+    const account = await this.account(this.account_name);
+    return {
+      ram_quota: account.ram_quota,
+      ram_usage: account.ram_usage,
+    };
+  };
+
+  this.all = async function () {
+    let result = [],
+      data;
+
+    do {
+      data = await this.rpc.get_table_rows({
+        json: true,
+        code: this.contract,
+        scope: this.account_name,
+        table: "keyval",
+        lower_bound: data && data.next_key,
+        limit: 100,
+      });
+      result.push(...data.rows);
+    } while (data.more);
+
+    return result;
+  };
+
+  this.get = async function (categoryId, key) {
+    if (categoryId == undefined || categoryId == null) {
+      throw new Error("categoryId required.");
+    }
+
+    if (!key) {
+      throw new Error("key required.");
+    }
+
+    const hash = eosECC.sha256(categoryId + "-" + key);
+    const data = await this.rpc.get_table_rows({
+      json: true,
+      code: this.contract,
+      scope: this.account_name,
+      table: "keyval",
+      lower_bound: hash,
+      upper_bound: hash,
+      limit: 1,
+      key_type: "sha256",
+      index_position: 2,
     });
 
-    this.keypair = async function() {
-        // generates a new (random) keypair 
-        return {
-            ownerPrivateKey: await eosECC.randomKey(),
-            activePrivateKey: await eosECC.randomKey()
-        }
+    if (data.rows.length === 0) {
+      throw new Error(`Key ${key} not found.`);
+    }
+
+    return data.rows[0];
+  };
+
+  this.add = async function (categoryId, key, value, authorization, options) {
+    authorization = authorization || this.defaultAuth;
+    options = {
+      broadcast: true,
+      sign: true,
+      blocksBehind: 0,
+      expireSeconds: 60,
+      ...options,
     };
+    return this.eos.transact(
+      {
+        actions: [
+          {
+            account: this.contract,
+            name: "insertkey",
+            authorization,
+            data: {
+              owner: this.account_name,
+              category_id: categoryId,
+              key,
+              value,
+            },
+          },
+        ],
+      },
+      options
+    );
+  };
 
-    this.verify = async function(privateKey, accountName) {
-        // verifies a given accountName can sign with privateKey
-        const account = await this.account(accountName);
-        const publicKey = eosECC.privateToPublic(privateKey);
-        return account.permissions.some( p => p.required_auth.keys.some( k => k.key == publicKey));
+  this.set = async function (categoryId, key, value, authorization, options) {
+    authorization = authorization || this.defaultAuth;
+    options = {
+      broadcast: true,
+      sign: true,
+      blocksBehind: 0,
+      expireSeconds: 60,
+      ...options,
     };
+    return this.eos.transact(
+      {
+        actions: [
+          {
+            account: this.contract,
+            name: "updatekey",
+            authorization,
+            data: {
+              owner: this.account_name,
+              category_id: categoryId,
+              key,
+              value,
+            },
+          },
+        ],
+      },
+      options
+    );
+  };
 
-    this.account = function() {
-        // returns an account
-        return this.rpc.get_account(this.account_name);
-    }
+  this.rekey = async function (
+    categoryId,
+    key,
+    new_key,
+    authorization,
+    options
+  ) {
+    authorization = authorization || this.defaultAuth;
+    options = {
+      broadcast: true,
+      sign: true,
+      blocksBehind: 0,
+      expireSeconds: 60,
+      ...options,
+    };
+    return this.eos.transact(
+      {
+        actions: [
+          {
+            account: this.contract,
+            name: "rekey",
+            authorization,
+            data: {
+              owner: this.account_name,
+              category_id: categoryId,
+              key,
+              new_key,
+            },
+          },
+        ],
+      },
+      options
+    );
+  };
 
-    this.ram = async function() {
-        // returns ram usage for an account
-        const account = await this.account(this.account_name);
-        return {
-            ram_quota: account.ram_quota,
-            ram_usage: account.ram_usage
-        }
-    }
+  this.delete = async function (categoryId, key, authorization, options) {
+    authorization = authorization || this.defaultAuth;
+    options = {
+      broadcast: true,
+      sign: true,
+      blocksBehind: 0,
+      expireSeconds: 60,
+      ...options,
+    };
+    return this.eos.transact(
+      {
+        actions: [
+          {
+            account: this.contract,
+            name: "deletekey",
+            authorization,
+            data: {
+              owner: this.account_name,
+              category_id: categoryId,
+              key,
+            },
+          },
+        ],
+      },
+      options
+    );
+  };
 
-    this.all = async function() {
-        let result = [], data;
+  // estimators (returns bytes needed)
+  const thisRoot = this;
+  this.estimate = {
+    add: function (key, value) {
+      return thisRoot.fixedRowRamCost + key.length + value.length;
+    },
 
-        do {
-            data = await this.rpc.get_table_rows({
-                json: true,
-                code: this.contract,
-                scope: this.account_name,
-                table: 'keyval',
-                lower_bound: data && data.next_key,
-                limit: 100
-            })
-            result.push(...data.rows);
-        } while(data.more);
+    set: async function (categoryId, key, value) {
+      const oldVal = await thisRoot.get(categoryId, key);
+      return key - oldVal.key + value - oldVal.value;
+    },
 
-        return result;
-    }
+    rekey: async function (categoryId, key, new_key) {
+      const oldVal = await thisRoot.get(categoryId, key);
+      return new_key - oldVal.key;
+    },
 
-    this.get = async function(categoryId, key) {
-        if(categoryId == undefined || categoryId == null) {
-            throw new Error("categoryId required.");
-        }
-
-        if(!key) {
-            throw new Error("key required.");
-        }
-
-        const hash = eosECC.sha256(categoryId + '-' + key); 
-        const data = await this.rpc.get_table_rows({
-            json: true,
-            code: this.contract,
-            scope: this.account_name,
-            table: 'keyval',
-            lower_bound: hash,
-            upper_bound: hash,
-            limit: 1,
-            key_type: 'sha256',
-            index_position: 2
-        });
-
-        if(data.rows.length === 0) {
-            throw new Error(`Key ${key} not found.`);
-        }
-
-        return data.rows[0];
-    }
-
-    this.add = async function(categoryId, key, value, authorization, options) {
-        authorization = authorization || this.defaultAuth
-        options = { broadcast: true, sign: true, blocksBehind: 0, expireSeconds: 60, ...options }
-        return this.eos.transact(
-        {
-            actions: [
-                {
-                    account: this.contract,
-                    name: 'insertkey',
-                    authorization,
-                    data: {
-                        owner: this.account_name,
-                        category_id: categoryId,
-                        key,
-                        value
-                    }
-                }
-            ]
-        }, options);
-    }
-
-    this.set = async function(categoryId, key, value, authorization, options) {
-        authorization = authorization || this.defaultAuth
-        options = { broadcast: true, sign: true, blocksBehind: 0, expireSeconds: 60, ...options }
-        return this.eos.transact(
-        {
-            actions: [
-                {
-                    account: this.contract,
-                    name: 'updatekey',
-                    authorization,
-                    data: {
-                        owner: this.account_name,
-                        category_id: categoryId,
-                        key,
-                        value
-                    }
-                }
-            ]
-        }, options);
-    }
-
-    this.rekey = async function(categoryId, key, new_key, authorization, options) {
-        authorization = authorization || this.defaultAuth
-        options = { broadcast: true, sign: true, blocksBehind: 0, expireSeconds: 60, ...options }
-        return this.eos.transact(
-        {
-            actions: [
-                {
-                    account: this.contract,
-                    name: 'rekey',
-                    authorization,
-                    data: {
-                        owner: this.account_name,
-                        category_id: categoryId,
-                        key,
-                        new_key
-                    }
-                }
-            ]
-        }, options);
-    }
-
-
-    this.delete = async function(categoryId, key, authorization, options) {
-        authorization = authorization || this.defaultAuth
-        options = { broadcast: true, sign: true, blocksBehind: 0, expireSeconds: 60, ...options }
-        return this.eos.transact(
-        {
-            actions: [
-                {
-                    account: this.contract,
-                    name: 'deletekey',
-                    authorization,
-                    data: {
-                        owner: this.account_name,
-                        category_id: categoryId,
-                        key
-                    }
-                }
-            ]
-        }, options);
-    }
-
-    // estimators (returns bytes needed)
-    const thisRoot = this;
-    this.estimate = {
-
-        add: function(key, value) {        
-            return thisRoot.fixedRowRamCost + key.length + value.length;
-        },
-
-        set: async function(categoryId, key, value) {
-            const oldVal = await thisRoot.get(categoryId, key);
-            return key - oldVal.key + value - oldVal.value;
-        },
-
-        rekey: async function(categoryId, key, new_key) {
-            const oldVal = await thisRoot.get(categoryId, key);
-            return new_key - oldVal.key;
-        },
-
-        delete: async function(categoryId, key) {
-            const oldVal = await thisRoot.get(categoryId, key);
-            return -(thisRoot.fixedRowRamCost + oldVal.key + oldVal.value);
-        }
-    }
-}
+    delete: async function (categoryId, key) {
+      const oldVal = await thisRoot.get(categoryId, key);
+      return -(thisRoot.fixedRowRamCost + oldVal.key + oldVal.value);
+    },
+  };
+};
--- tests/putjs.test.js
+++ tests/putjs.test.js
@@ -1,185 +1,186 @@
-const Putjs = require('../src/index.js')
-const assert = require('assert');
+const Putjs = require("../src/index.js");
+const assert = require("assert");
 
-const EOS_ENDPOINT = "http://localhost:8888"
-const CONTRACTS_KEY_PRIV = "5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr"
+const EOS_ENDPOINT = "http://localhost:8888";
+const CONTRACTS_KEY_PRIV =
+  "5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr";
 
 function debugResults(results) {
-  for(let i = 0; i < results.length; i++) {
-    console.log(`------ debug out ${i} ------`)
+  for (let i = 0; i < results.length; i++) {
+    console.log(`------ debug out ${i} ------`);
     console.log(results[i]);
-    console.log(`--------------------------`)
+    console.log(`--------------------------`);
   }
 }
 
 function newUtcDateString(addSeconds) {
-  const now = new Date()
-  now.setSeconds(now.getSeconds() + addSeconds)
+  const now = new Date();
+  now.setSeconds(now.getSeconds() + addSeconds);
 
-  var n = now.toISOString()
-  if (n.substr(23, 1) == 'Z') {
-      n = n.substr(0, 23);
+  var n = now.toISOString();
+  if (n.substr(23, 1) == "Z") {
+    n = n.substr(0, 23);
   }
-  return n
+  return n;
 }
 
-describe('Put contract', () => {
-    before(async function() {
-        this.timeout(0);
-    })
+describe("Put contract", () => {
+  before(async function () {
+    this.timeout(0);
+  });
 
-    after(async function() {
-        this.timeout(0);
-    })
+  after(async function () {
+    this.timeout(0);
+  });
 
-    it('Generate keys', async () => {
-      const put = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Generate keys", async () => {
+    const put = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result = await put.keypair();
-      debugResults([{randomKeys:result}]);
-    })
+    const result = await put.keypair();
+    debugResults([{ randomKeys: result }]);
+  });
 
-    it('check private key in account', async () => {
-      const put = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("check private key in account", async () => {
+    const put = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result = await put.verify(CONTRACTS_KEY_PRIV, 'putuseruser1');
-      debugResults([{verify:result}]);
-    })
+    const result = await put.verify(CONTRACTS_KEY_PRIV, "putuseruser1");
+    debugResults([{ verify: result }]);
+  });
 
-    it('Insert key', async () => {
-      const put1 = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Insert key", async () => {
+    const put1 = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result = await put1.ram();
-      debugResults([{accram1:result}]);
+    const result = await put1.ram();
+    debugResults([{ accram1: result }]);
 
-      await put1.add(0, 'flag1', 'true');
+    await put1.add(0, "flag1", "true");
 
-      const result1 = await put1.ram();
-      debugResults([{accram2:result1}]);
+    const result1 = await put1.ram();
+    debugResults([{ accram2: result1 }]);
 
-      await put1.add(0, 'flag2', 'false');
+    await put1.add(0, "flag2", "false");
 
-      const result2 = await put1.ram();
-      debugResults([{accram3:result2}]);
+    const result2 = await put1.ram();
+    debugResults([{ accram3: result2 }]);
 
-      await put1.add(0, 'max_signups', '100');
+    await put1.add(0, "max_signups", "100");
 
-      const result3 = await put1.ram();
-      debugResults([{accram3:result3}]);
+    const result3 = await put1.ram();
+    debugResults([{ accram3: result3 }]);
 
-      await put1.add(0, 'signup_uri', 'https://example.tld');
+    await put1.add(0, "signup_uri", "https://example.tld");
 
-      const result4 = await put1.ram();
-      debugResults([{accram3:result4}]);
+    const result4 = await put1.ram();
+    debugResults([{ accram3: result4 }]);
 
-      const put2 = new Putjs({
-        account_name: 'putuseruser2',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+    const put2 = new Putjs({
+      account_name: "putuseruser2",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      await put2.add(0, 'encrypted_hash', '4db268bbaad225a0a');
-    })
+    await put2.add(0, "encrypted_hash", "4db268bbaad225a0a");
+  });
 
-    it('Update key', async () => {
-      const put1 = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Update key", async () => {
+    const put1 = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      await put1.set(0, 'max_signups', '200');
-    })
+    await put1.set(0, "max_signups", "200");
+  });
 
-    it('Rekey', async () => {
-      const put1 = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Rekey", async () => {
+    const put1 = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      await put1.rekey(0, 'max_signups', 'max_signups2');
-    })
+    await put1.rekey(0, "max_signups", "max_signups2");
+  });
 
-    it('Delete key', async () => {
-      const put1 = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Delete key", async () => {
+    const put1 = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      await put1.delete(0, 'max_signups2');
-    })
+    await put1.delete(0, "max_signups2");
+  });
 
-    it('Fetch All keys', async () => {
-      const put1 = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Fetch All keys", async () => {
+    const put1 = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result = await put1.all();
+    const result = await put1.all();
 
-      const put2 = new Putjs({
-        account_name: 'putuseruser2',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+    const put2 = new Putjs({
+      account_name: "putuseruser2",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result1 = await put2.all();
-      debugResults([{all:result}, {all1:result1}]);
-    })
+    const result1 = await put2.all();
+    debugResults([{ all: result }, { all1: result1 }]);
+  });
 
-    it('Fetch single key', async () => {
-      const put1 = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Fetch single key", async () => {
+    const put1 = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result1 = await put1.get(0, 'signup_uri');
+    const result1 = await put1.get(0, "signup_uri");
 
-      const put2 = new Putjs({
-        account_name: 'putuseruser2',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+    const put2 = new Putjs({
+      account_name: "putuseruser2",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result2 = await put2.get(0, 'encrypted_hash');
-      debugResults([{key1:result1}, {key2: result2}]);
-    })
+    const result2 = await put2.get(0, "encrypted_hash");
+    debugResults([{ key1: result1 }, { key2: result2 }]);
+  });
 
-    it('Get account', async () => {
-      const put = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Get account", async () => {
+    const put = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result = await put.account();
-      debugResults([{account:result}]);
-    })
+    const result = await put.account();
+    debugResults([{ account: result }]);
+  });
 
-    it('Get ram usage', async () => {
-      const put = new Putjs({
-        account_name: 'putuseruser1',
-        eos_endpoint: EOS_ENDPOINT,
-        account_payer_pk: CONTRACTS_KEY_PRIV
-      });
+  it("Get ram usage", async () => {
+    const put = new Putjs({
+      account_name: "putuseruser1",
+      eos_endpoint: EOS_ENDPOINT,
+      account_payer_pk: CONTRACTS_KEY_PRIV,
+    });
 
-      const result = await put.ram();
-      debugResults([{accram:result}]);
-    })
-  })
+    const result = await put.ram();
+    debugResults([{ accram: result }]);
+  });
+});
